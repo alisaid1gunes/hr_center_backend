ec0f275d8463e21bab83b54d2ed02b443fd64ccc
alisaidgunes <alisaidgunes1@gmail.com>
2022-08-10 15:21:34 +0300

    separate dev and production database connection configs in data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,25 @@ import { createConnection } from "typeorm";
 import { User } from "./users/entities/user.entity";
 
 export const connectDB = async () => {
-  try {
-    await createConnection({
-      type: "postgres",
+  let source = {};
+  if (process.env.NODE_ENV === "production") {
+    source = {
+      url: process.env.DATABASE_URL,
+    };
+  } else if (process.env.NODE_ENV === "development") {
+    source = {
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+    };
+  }
+
+  try {
+    await createConnection({
+      type: "postgres",
+      ...source,
       logging: false,
       entities: [User],
       migrations: ["src/migrations/*.ts"],
